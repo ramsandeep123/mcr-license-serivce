13d165c827b731a14f3e297dea1c8b0c71c5b596
ramsandeep123 <ramsandeep90900@gmail.com>
2025-09-07 23:22:20 +0530

    added postinstall newr

--- sky.js
+++ sky.js
@@ -12,7 +12,7 @@ async function createSkySlopeAgent(firstName,lastName,email,streetNo,streetName,
    const browser = await puppeteer.launch({ 
     headless: "new", // Use the new headless mode
     defaultViewport: null,
-    // executablePath: executablePath(), // Specify executable path for Render
+    executablePath: executablePath(), // Specify executable path for Render
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
